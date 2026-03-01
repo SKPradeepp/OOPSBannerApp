9b92adbfd8620600b0f2ce1741f4d42b4963aaa0
SKPradeepp <skpiitmember2008@gmail.com>
2026-03-01 17:52:48 +0530

    Added UC5 Inline Array Initialization implementation

---
+++ src/OOPSBannerAppUC5.java
@@ -0,0 +1,56 @@
+public class OOPSBannerAppUC5 {
+
+    public static void main(String[] args) {
+
+        // Inline array declaration + initialization
+        String[] banner = {
+
+            String.join(" ",
+                    " *****  ",
+                    " *****  ",
+                    " ****** ",
+                    " ****** "),
+
+            String.join(" ",
+                    "*     * ",
+                    "*     * ",
+                    "*     * ",
+                    "*     * "),
+
+            String.join(" ",
+                    "*     * ",
+                    "*     * ",
+                    "*     * ",
+                    "*       "),
+
+            String.join(" ",
+                    "*     * ",
+                    "*     * ",
+                    " ****** ",
+                    " *****  "),
+
+            String.join(" ",
+                    "*     * ",
+                    "*     * ",
+                    "*       ",
+                    "      * "),
+
+            String.join(" ",
+                    "*     * ",
+                    "*     * ",
+                    "*       ",
+                    "*     * "),
+
+            String.join(" ",
+                    " *****  ",
+                    " *****  ",
+                    "*       ",
+                    " *****  ")
+        };
+
+        // Enhanced for loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
